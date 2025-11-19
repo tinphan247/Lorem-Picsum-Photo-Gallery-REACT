61a67ae0b8aa6c681253002a7ba4ad8692d1d301
tinphan247 <tinphan111005@gmail.com>
2025-11-19 21:19:41 +0700

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Cần thay thế 'Lorem-Picsum-Photo-Gallery-REACT' bằng tên repository của bạn (Nếu bạn đã đổi tên)
+const REPO_NAME = 'Lorem-Picsum-Photo-Gallery-REACT'; 
+
 export default defineConfig({
-  plugins: [
-    react({
-      babel: {
-        plugins: [['babel-plugin-react-compiler']],
-      },
-    }),
-  ],
+  plugins: [react()],
+  // Thiết lập đường dẫn gốc (base path) để tải đúng tài nguyên trên GitHub Pages
+  // Nếu bạn triển khai trên một miền tùy chỉnh hoặc trên main domain, bạn có thể bỏ 'base'
+  base: `/${REPO_NAME}/`, 
 })
